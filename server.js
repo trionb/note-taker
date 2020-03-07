3bfedb217ac93e02b3efa7ccc2c1527630779dfc
trion bailey <trionbailey@gmail.com>
2020-03-06 19:26:09 -0500

    Delete working

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 let express = require("express");
 let path = require("path");
-let db =require("./db/db.json");
-const { v4: uuidv4 } = require('uuid');
-
+let db = require("./db/db.json");
+//const { v4: uuidv4 } = require('uuid');
+let newId = 1;
 
 
 // Sets up the Express App
 let app = express();
-var PORT = process.env.PORT||8080;
+var PORT = process.env.PORT || 8000;
 
 
 // Sets up the Express app to handle data parsing
@@ -17,8 +17,8 @@ app.use(express.json());
 //makes local host look in the publick folder on client side
 app.use(express.static("public"));
 
-app.listen(PORT,function(){
-    console.log("app listening Port"+PORT)
+app.listen(PORT, function () {
+    console.log("app listening Port" + PORT)
 })
 
 // Basic route that sends the user first to the AJAX Page
@@ -30,19 +30,30 @@ app.get("/notes", function (req, res) {
 });
 
 // //read db.json file and return saved notes as JSON
-app.get("/api/notes",function(req,res){
+app.get("/api/notes", function (req, res) {
+
     return res.json(db);
 })
 
-//receive a new note and return new note ti the client
-app.post("/api/notes", function(req,res){
+//receive a new note and return new note to the client
+app.post("/api/notes", function (req, res) {
+    db.push(req.body)
+    //req.body.id = db.length
+    newId = db.length + 1
+    // console.log(newId)
     return res.json(db);
 })
 // //should receive a query parameter with id 
-app.delete(`/api/notes/:id`, function(req,res){
-    var noteChosen= req.params.id;
-    //if(let i =0; i<)
-    return res.Json(db);
-})
+app.delete(`/api/notes/:id`, function (req, res) {
+    // let noteChosen = req.param.id
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id === parseInt(req.params.id)) {
+            db.splice((i), 1);
+        }
+    }
 
-uuidv4();
+    return res.json(db);
+});
+
+
+//uuidv4();
